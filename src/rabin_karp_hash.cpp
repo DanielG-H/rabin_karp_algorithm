0b48a226f170527e90bf8643f5faa2f077464d37
Danny <danielgarcia.alumno@gmail.com>
2022-05-12 10:15:47 -0700

    Update to printFile method

--- src/rabin_karp_hash.cpp
+++ src/rabin_karp_hash.cpp
@@ -10,15 +10,13 @@ void rabin_karp( char* palabra, char* linea, int numero_linea){
 void printFile()
 {
     char ch;
-    char* line;
-    int lineNumber = 1;
+    int lineNumber = 0;
     string l;
     fstream fin("file/sample.txt", fstream::in);
     while(fin >> noskipws >> ch){
         l += ch;
         if(ch == '\n') {   // detects the end of the line
-            line = &l[0];
-            cout << *line;
+            cout << l;
             l = "";
             lineNumber++;
         }
