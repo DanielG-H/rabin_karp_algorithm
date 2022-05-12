f7756f1df7204657a38b6a8ae887b571c4598aea
Danny <danielgarcia.alumno@gmail.com>
2022-05-12 14:52:37 -0700

    Update readFileAndFindWord to find_word_matches, standarizing to snake_case. Implemented a class RabinKarpAlgo which contains all the methos for the file searching

--- src/rabin_karp_hash.cpp
+++ src/rabin_karp_hash.cpp
@@ -1,38 +1,47 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <string.h>
 #include <fstream>
 #include <alloca.h>
 #include <cstring>
 using namespace std;
 
-void rabin_karp( char* palabra, char* linea, int numero_linea){
-    //
-}
-
-void readFileAndFindWord(char* word){
-    char ch;
-    int lineNumber = 0;
-    string l;
-    fstream fin("file/sample.txt", fstream::in);
-    while(fin >> noskipws >> ch){
-        l += ch;
-        if(ch == '\n') {   // detects the end of the line
-            char* line;
-
-            line = (char *)alloca(l.size() + 1);
-            memcpy(line, l.c_str(), l.size() + 1);
-
-            lineNumber++;
-
-            rabin_karp(word, line, lineNumber);
-
-            l = "";
+class RabinKarpAlgo{
+    public:
+        static void rabin_karp( char* palabra, char* linea, int numero_linea){
+            //
         }
-    }
-    fin.close();
+
+        static void readFileAndFindWord(char* word, string file){
+            char ch;
+            int lineNumber = 0;
+            string l; //temporal string to append characters
+
+            fstream fin(file, fstream::in);
+
+            while(fin >> noskipws >> ch){
+                l += ch;
+                if(ch == '\n'){   // detects the end of the line
+                    char* line;
+
+                    line = (char *)alloca(l.size() + 1); // allocates memory on stack and copies string
+                    memcpy(line, l.c_str(), l.size() + 1);
+
+                    lineNumber++;
+
+                    rabin_karp(word, line, lineNumber);
+
+                    l = "";
+                }
+            }
+            fin.close();
+        }
+    private:
+        int hashFunction(char c){
+            return 0;
+        }
 }
 
-int main()
-{
+int main(){
     return 0;
 }
