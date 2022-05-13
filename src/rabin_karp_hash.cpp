76a1884c601ebf51e279701dd0c30f80a2767514
Danny <danielgarcia.alumno@gmail.com>
2022-05-12 18:58:57 -0700

    Update organization of methods in class and added some variables in hash_function

--- src/rabin_karp_hash.cpp
+++ src/rabin_karp_hash.cpp
@@ -7,40 +7,43 @@
 using namespace std;
 
 class RabinKarpAlgo{
+    private:
+        static int hash_function(char* palabra, int length_palabra){
+            const int prime = 11;
+            const int number_of_ascii_chars = 95;
+            int hash = 0;
+            int weight_char = 0;
+            
+            return 0;
+        }
+        
     public:
         static void rabin_karp( char* palabra, char* linea, int numero_linea){
-            //
+            const int length_palabra = strlen(palabra);
+            const int length_linea = strlen(linea);
+            
+            cout << hash_function(palabra, length_palabra);
         }
-
-        static void readFileAndFindWord(char* word, string file){
+        
+        static void findWordMatches(char* word, string file){
             char ch;
             int lineNumber = 0;
-            string l; //temporal string to append characters
-
+            string l;
             fstream fin(file, fstream::in);
-
             while(fin >> noskipws >> ch){
                 l += ch;
-                if(ch == '\n'){   // detects the end of the line
+                if(ch == '\n') {   // detects the end of the line
                     char* line;
-
-                    line = (char *)alloca(l.size() + 1); // allocates memory on stack and copies string
+                    line = (char *)alloca(l.size() + 1);
                     memcpy(line, l.c_str(), l.size() + 1);
-
                     lineNumber++;
-
                     rabin_karp(word, line, lineNumber);
-
                     l = "";
                 }
             }
             fin.close();
         }
-    private:
-        int hashFunction(char c){
-            return 0;
-        }
-}
+};
 
 int main(){
     return 0;
