b7f0980915d9ba15f7f62f2dfc632540536b3708
Danny <danielgarcia.alumno@gmail.com>
2022-05-17 23:51:27 -0700

    Final version

--- src/rabin_karp_hash.cpp
+++ src/rabin_karp_hash.cpp
@@ -3,81 +3,83 @@
 #include <string.h>
 #include <fstream>
 #include <malloc.h>
+#include <vector>
+#include <tuple>
 #include <cstring>
-#include <math.h>
 
 using namespace std;
 
 class RabinKarpAlgo{
     private:
-        static const int PRIME = 331;
-        static const int NUM_OF_ASCII_CHARS = 95;
-        static const int NUM_TO_SUBSTRCT_TO_GET_WEIGHT = 31;
-        
+        const int PRIME = 77777;
+        const int BASE = 256;
     public:
-        static void rabin_karp( char* palabra, char* linea, int numero_linea){
+         /*
+            vector of tuples to store index of word in line and line number 
+            where it was found
+            */
+        vector< tuple<int, int> > list_of_instances;
+        
+        void rabin_karp(char* palabra, char* linea, int numero_linea){
             const int LENGTH_PALABRA = strlen(palabra);
             const int LENGTH_LINEA = strlen(linea);
+            tuple<int,int> indexes;
+            int hash_pow = 1;
             int hashed_palabra = 0;
             int hashed_window = 0; // window is the initial substring to be compared with word
-            int left_slider = 0; // index where substring starts and possible index of match
-            int right_slider = LENGTH_PALABRA-1; // index where substring ends
-            bool line_end = false, match_found = false;
+            int slider = 0; // index where substring starts and possible index of match
+            bool line_end = false;
             
-            int weight_char = 0;
-            int weight_char_2 = 0;
-            int power = 0;
-            
-            for (int i = 0; i < LENGTH_PALABRA; i++){
-                weight_char = (int)palabra[i]-NUM_TO_SUBSTRCT_TO_GET_WEIGHT;
-                hashed_palabra = hashed_palabra + weight_char * pow(NUM_OF_ASCII_CHARS, LENGTH_PALABRA-(i+1));
-
-                weight_char = (int)linea[i]-NUM_TO_SUBSTRCT_TO_GET_WEIGHT;
-                hashed_window = hashed_window + weight_char * pow(NUM_OF_ASCII_CHARS, LENGTH_PALABRA-(i+1));
+            // gets the exponents of hash (to not compute later)
+            for (int i = 0; i < LENGTH_PALABRA-1; i++){
+                hash_pow = (hash_pow*BASE) % PRIME;
             }
-            hashed_palabra = hashed_palabra % PRIME;
-            hashed_window = hashed_window % PRIME;
-
-            cout << hashed_palabra << endl;
-            cout << hashed_window << endl << endl;
             
-            while(!line_end && !match_found){
-                if(hashed_palabra == hashed_window){
-                    match_found = true;
-                    cout << endl << left_slider;
-                }else{
-                    left_slider++;
-                    right_slider++;
-                    weight_char = (int)linea[left_slider]-NUM_TO_SUBSTRCT_TO_GET_WEIGHT;
-                    weight_char_2 = (int)linea[right_slider]-NUM_TO_SUBSTRCT_TO_GET_WEIGHT;
-                    power =  pow(NUM_OF_ASCII_CHARS, LENGTH_PALABRA-1);
-                    hashed_window = (hashed_window - (weight_char * power  % PRIME)) * NUM_OF_ASCII_CHARS + weight_char_2;
-                    cout << hashed_window << endl;
-
-                    if(right_slider == LENGTH_LINEA-1) line_end = true;
+            // hashes the word and line 
+            for (int j = 0; j < LENGTH_PALABRA; j++){
+                hashed_palabra = (hashed_palabra*BASE + palabra[j]) % PRIME;
+                hashed_window = (hashed_window*BASE + linea[j]) % PRIME;
+            }
+            
+            // searches for word and switches window 
+            while (!line_end){
+                if (hashed_palabra == hashed_window){
+                    indexes = make_tuple(slider, numero_linea);
+                    list_of_instances.push_back(indexes);
+                    cout << "index: " << slider << " " << "line: " << numero_linea;
                 }
+                
+                hashed_window = hashed_window - (linea[slider] * hash_pow);
+                hashed_window = hashed_window * BASE;
+                hashed_window = (hashed_window + linea[slider+LENGTH_PALABRA]) % PRIME;
+                    
+                if (hashed_window < 0) hashed_window = hashed_window + PRIME;
+                
+                slider++;
+
+                if (slider > LENGTH_LINEA-LENGTH_PALABRA) line_end = true;
             } 
 
             
         }
         
-        static void find_word_matches(char* word, string file){
+        void rabin_karp_implementation(char* word, string file){
             char ch;
             int lineNumber = 0;
             string l;
 
             fstream fin(file, fstream::in);
 
-            while(fin >> noskipws >> ch){
+            while (fin >> noskipws >> ch){
                 l += ch;
-                if(ch == '\n') {   // detects the end of the line
+                if (ch == '\n' ) {   // detects the end of the line
                     char* line;
 
                     line = (char *)alloca(l.size() + 1); // allocates memory on the stack and copies string to char*
                     memcpy(line, l.c_str(), l.size() + 1);
 
                     lineNumber++;
-                    rabin_karp(word, line, lineNumber);
+                    if (strlen(line) >= strlen(word)) rabin_karp(word, line, lineNumber);
 
                     l = "";
                 }
@@ -87,6 +89,13 @@ class RabinKarpAlgo{
 };
 
 int main(){
-    RabinKarpAlgo::rabin_karp("br","Cumbres",1);
+    RabinKarpAlgo rk;
+
+    // arguments for rabin_karp_implementation that reads files: word, file
+    rk.rabin_karp_implementation("Pink","sample.txt");
+    
+    // arguments for rabin_karp: word, line, line_number
+    // rk.rabin_karp("","",0);
+    
     return 0;
 }
